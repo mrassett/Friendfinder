ab85f9262c3c0f513f06a4c90fa76e96d054ae55
mrassett <magsrassett@gmail.com>
2018-05-15 17:52:20 -0500

    not complete

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,5 +2,58 @@
 //friends is a data 
 
 // const friends is an array of objects, then you export the array 
-//BLANK 
-//mocking a server, your not 
+// Data structure that will store the friends data
+const friendsArray = [
+	{
+		name: 'Natalia',
+		photo: 'http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-15.jpg',
+		scores: [
+			'3',
+			'5',
+			'2',
+			'1'
+		]
+	},
+	{
+		name: 'Chanel',
+		photo: 'https://i.ytimg.com/vi/mRf3-JkwqfU/hqdefault.jpg',
+		scores: [
+			'4',
+			'3',
+			'2',
+			'1'
+		]
+	},
+	{
+		name: 'Ariel',
+		photo: 'http://cdn3-www.cattime.com/assets/uploads/2011/08/best-kitten-names-1.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Malibu',
+		photo: 'https://pbs.twimg.com/profile_images/562466745340817408/_nIu8KHX.jpeg',
+		scores: [
+			'5',
+			'5',
+			'2',
+			'4'
+		]
+	},
+	{
+		name: 'Lexi',
+		photo: "./public/images/Girl_1.png",
+		scores: [
+			'1',
+			'3',
+			'1',
+			'4'
+		]
+	},
+];
+
+module.exports = friendsArray;
